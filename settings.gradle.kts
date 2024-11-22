64b34ba15efaadee9dff725bdccbf0f7bfd0937e
Kuan-Ying Chou <kuanyingchou@google.com>
2024-11-22 16:51:17 +0000

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://androidx.dev/snapshots/builds/12682683/artifacts/repository")
+        }
+    }
+}
+
+rootProject.name = "TestNav3"
+include(":app")
+ 
